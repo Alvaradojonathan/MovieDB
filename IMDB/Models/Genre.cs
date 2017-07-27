cc9c44ab61cda8c07021292fcd1344371fadbbdb
Your Name <you@example.com>
2017-07-27 09:00:03 -0400

    added models for actors, movies and genres

---
+++ IMDB/Models/Genre.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace IMDB.Models
+{
+    public class Genre
+    {
+        [Key]
+        public int GenreID;
+        public string GenreName;
+
+        public virtual ICollection<Movie> Movies { get; set; }
+    }
+}
